28d5090f97c15e342d9ed2fd7132da92c4a4e620
EligiblePeace01 <enwerempeace@gmail.com>
2024-03-06 17:31:20 -0800

    make changes to my code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+'''queries the Reddit API and returns the number of subscribers of subreddit'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    ''' get num of subs '''
+
+    base_url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-agent': 'timex19'}
+    res = requests.get(base_url, headers=headers)
+
+    if res.status_code == 200:
+        subs_num = res.json().get('data').get('subscribers')
+        return subs_num
+    else:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
